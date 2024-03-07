54925898c31db02ee2e574c65b5db28ac8f31136
fisherlyon <fwlyon@calpoly.edu>
2024-03-07 14:53:41 -0800

    maven

---
+++ src/main/java/com/final_project_csc308_winter_2024/App.java
@@ -0,0 +1,13 @@
+package com.final_project_csc308_winter_2024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/final_project_csc308_winter_2024/AppTest.java
@@ -0,0 +1,20 @@
+package com.final_project_csc308_winter_2024;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
