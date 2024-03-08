26f1b188c6fd5fc1fc1a1310bf29b966df745207
fisherlyon <fwlyon@calpoly.edu>
2024-03-07 16:32:55 -0800

    action listener in App

--- src/main/java/com/final_project_csc308_winter_2024/App.java
+++ src/main/java/com/final_project_csc308_winter_2024/App.java
@@ -1,8 +1,11 @@
 package com.final_project_csc308_winter_2024;
 
 import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class App extends JFrame {
+public class App extends JFrame implements ActionListener {
 
     public App() {
         System.out.println("Hello World!");
@@ -10,5 +13,11 @@ public class App extends JFrame {
     
     public static void main( String[] args ) {
         App main = new App();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 }
