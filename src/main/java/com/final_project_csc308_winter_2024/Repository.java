184f254001ea383794cf2eddc11f36930826da63
fisherlyon <fwlyon@calpoly.edu>
2024-03-08 20:59:43 -0800

    first repo push

---
+++ src/main/java/com/final_project_csc308_winter_2024/Repository.java
@@ -0,0 +1,41 @@
+package com.final_project_csc308_winter_2024;
+
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+
+public class Repository extends PropertyChangeSupport {
+
+    private static Repository instance = null;
+    private int counter = 0;
+    private Tower[] towers = new Tower[3];
+
+    private Repository() {
+        super(new Object());
+    }
+
+    public static Repository getInstance(){
+        if (instance == null){
+            instance = new Repository();
+        }
+        return instance;
+    }
+
+    public void move(int old_tower, int new_tower) {
+        counter += 1;
+        Disk disk = towers[old_tower].peek();
+        if (disk.getSize() < towers[new_tower].peek().getSize()) {
+            towers[old_tower].pop();
+            towers[new_tower].push(disk);
+            firePropertyChange("counter", null, counter);
+            firePropertyChange("towers", null, towers);
+        }
+    }
+
+    public int getCount() {
+        return counter;
+    }
+
+    public Tower[] getTowers() {
+        return towers;
+    }
+}
