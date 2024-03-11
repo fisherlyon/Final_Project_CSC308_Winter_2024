551a5495085ed2528b112085e38fa65ee5f86b46
Jonathan Jara <jonathan.jara.2000@gmail.com>
2024-03-10 23:46:45 -0700

    added TowerPanel, won't work for sure until tested with actual towers and disks

---
+++ src/main/java/com/final_project_csc308_winter_2024/TowerPanel.java
@@ -0,0 +1,26 @@
+package com.final_project_csc308_winter_2024;
+
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class TowerPanel extends JPanel implements PropertyChangeListener {
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Tower[] towers = Repository.getInstance().getTowers();
+        for (int i = 0; i < 3; i++) {
+            towers[i].draw(g, i);
+            for(Disk disk : towers[i].getDisks()){
+                disk.draw(g, i);
+            }
+        }
+    }
+}
