10bc7ff2a0c6c5029a2b9e22979494726ef2014d
Jonathan Jara <jonathan.jara.2000@gmail.com>
2024-03-12 15:27:05 -0700

    pushed TutorPanel

---
+++ src/main/java/com/final_project_csc308_winter_2024/TutorPanel.java
@@ -0,0 +1,18 @@
+package com.final_project_csc308_winter_2024;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TutorPanel extends JPanel {
+    @Override
+    public void repaint() {
+        super.repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Tutor tutor = new Tutor();
+        tutor.draw(g);
+    }
+}
