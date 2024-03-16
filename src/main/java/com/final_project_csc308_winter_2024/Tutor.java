5737a5ac96c565113e466bdda0498c60b9e9f428
Jonathan Jara <jonathan.jara.2000@gmail.com>
2024-03-15 21:01:20 -0700

    pushed TutorPanel and Tutor

--- src/main/java/com/final_project_csc308_winter_2024/Tutor.java
+++ src/main/java/com/final_project_csc308_winter_2024/Tutor.java
@@ -3,6 +3,13 @@ package com.final_project_csc308_winter_2024;
 import java.awt.*;
 
 public class Tutor {
+    int x = 100;
+    int y = 200;
+    int width = 400;
+    int height = 100;
+
+    int[] tailX = {480, 480, 530};
+    int[] tailY = {280, 300, 300};
 
     public Tutor() {
         System.out.println("Hello From Tutor!");
@@ -11,12 +18,16 @@ public class Tutor {
     public void draw(Graphics g) {
         System.out.println("Drawing Tutor!");
         g.setColor(Color.WHITE);
-        g.fillRoundRect(100, 200, 400, 100, 20, 20); // Bubble rectangle with rounded corners
-
+        g.fillRoundRect(x, y, width, height, 20, 20); // Bubble rectangle with rounded corners
         // Draw the "tail" of the bubble
-        int[] tailX = {150, 140, 150}; // X coordinates for the tail
-        int[] tailY = {250, 260, 270}; // Y coordinates for the tail
         g.fillPolygon(tailX, tailY, 3); // Triangle for the tail
+        int f =  x + (width /4);
+        int h = y + (height / 2);
+        g.setColor(Color.BLACK);
+        int disk = Repository.getInstance().getNextStep()[0] +1;
+        int from = Repository.getInstance().getNextStep()[1] +1;
+        int to = Repository.getInstance().getNextStep()[2] +1;
+        g.drawString("Move disk " + disk + " from tower " + from + " to tower " + to, f, h);
     }
 
 }
--- src/main/java/com/final_project_csc308_winter_2024/TutorPanel.java
+++ src/main/java/com/final_project_csc308_winter_2024/TutorPanel.java
@@ -2,8 +2,14 @@ package com.final_project_csc308_winter_2024;
 
 import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class TutorPanel extends JPanel {
+public class TutorPanel extends JPanel implements PropertyChangeListener {
+
+    public TutorPanel() {
+        Repository.getInstance().addPropertyChangeListener(this);
+    }
     @Override
     public void repaint() {
         super.repaint();
@@ -12,9 +18,17 @@ public class TutorPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Tutor tutor = new Tutor();
-        tutor.draw(g);
+//        if (Repository.getInstance().getCount() == 0) {
+//            System.out.println("No moves yet");
+//        }
+//        else{
+            Tutor tutor = new Tutor();
+            tutor.draw(g);
+//        }
     }
 
-
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        repaint();
+    }
 }
