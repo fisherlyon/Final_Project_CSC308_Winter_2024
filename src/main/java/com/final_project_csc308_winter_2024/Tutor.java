2484e23bac17d7edd83f8e19004f9bacb4977c62
Cloudy <dutran.pgm@gmail.com>
2024-03-16 02:18:23 -0700

    working on tutor messages (depending on game),added tutor png, tutor prints messages when clicked on (currently just the initial message)

--- src/main/java/com/final_project_csc308_winter_2024/Tutor.java
+++ src/main/java/com/final_project_csc308_winter_2024/Tutor.java
@@ -1,15 +1,22 @@
 package com.final_project_csc308_winter_2024;
 
 import java.awt.*;
+import javax.swing.*;
 
-public class Tutor {
+public class Tutor{
+    private Image tutorImage;
+    private String message = ""; // Message to be displayed in the speech bubble
 
     public Tutor() {
-        System.out.println("Hello From Tutor!");
+        // Load the image. Du Tran
+        ImageIcon ii = new ImageIcon("/Users/cloudyfat/final_project_csc308_winter_2024/src/main/resources/tutor1.png");
+        tutorImage = ii.getImage();
+        // End of Du
+        //System.out.println("Hello From Tutor!");
     }
 
     public void draw(Graphics g) {
-        System.out.println("Drawing Tutor!");
+        //System.out.println("Drawing Tutor!");
         g.setColor(Color.WHITE);
         g.fillRoundRect(100, 200, 400, 100, 20, 20); // Bubble rectangle with rounded corners
 
@@ -17,6 +24,30 @@ public class Tutor {
         int[] tailX = {150, 140, 150}; // X coordinates for the tail
         int[] tailY = {250, 260, 270}; // Y coordinates for the tail
         g.fillPolygon(tailX, tailY, 3); // Triangle for the tail
+
+        // Du Tran's tutor image guy for fun cuase i was eepy and this was at 3am
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        if (tutorImage != null) {
+            g2d.drawImage(tutorImage, 400, 100, 350, 400, null);
+        }
+
+        // Draw the message text if it's not empty
+        if (!message.isEmpty()) {
+            g2d.setColor(Color.BLACK);
+            g2d.setFont(new Font("Arial", Font.BOLD, 20));
+            FontMetrics fm = g2d.getFontMetrics();
+            int textWidth = fm.stringWidth(message);
+            int x = 100 + (400 - textWidth) / 2;
+            int y = 200 + ((100 - fm.getHeight()) / 2) + fm.getAscent();
+            g2d.drawString(message, x, y);
+        }
     }
 
+    // Method to set the message
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    //End of Du's work
 }
--- src/main/java/com/final_project_csc308_winter_2024/TutorPanel.java
+++ src/main/java/com/final_project_csc308_winter_2024/TutorPanel.java
@@ -2,6 +2,8 @@ package com.final_project_csc308_winter_2024;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TutorPanel extends JPanel {
     @Override
@@ -12,9 +14,27 @@ public class TutorPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Tutor tutor = new Tutor();
+        // Use the existing 'tutor' instance instead of creating a new one.
         tutor.draw(g);
     }
 
+    //DU Tran stuff to click the tutor for help
+    private Tutor tutor;
+    private final Rectangle tutorBounds = new Rectangle(400, 100, 350, 400);
 
+    public TutorPanel() {
+        this.tutor = new Tutor(); // Initialize the tutor once here
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (tutorBounds.contains(e.getPoint())) {
+                    tutor.setMessage("I'm Javier, your tutor"); // Set the message
+                    System.out.println("Tutor image clicked! Setting message.");
+                    revalidate();
+                    repaint();
+                }
+            }
+        });
+    }
+    //End of Du
 }
